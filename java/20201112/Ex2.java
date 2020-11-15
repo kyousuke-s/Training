17ff143d6b7938275ffed686dd3f86ede35f8a92
kyou <kyousukesato8@gmail.com>
2020-11-16 02:07:56 +0900

    Ex2.java編集

--- java/20201112/Ex2.java
+++ java/20201112/Ex2.java
@@ -1,50 +1,38 @@
 import java.util.*;
 public class Ex2{
-	public static void main(String[] args){
-		//要素数の入力、配列の作成
-		System.out.print("要素数を入力>");
-		int youso=new Scanner(System.in).nextInt();
-		int[] nums=new int[youso];
-		//カウントする値、カウント数
-		int[] target={1,7,17};
-		int[] count={0,0,0};
-		//乱数作成の繰り返し
-		for(int i=0;i<nums.length;i++){
-			int num=new Random().nextInt(nums.length);
-			//目的の値であるかどうか
-			if(num==target[0]){
-				//配列に格納
-				nums[i]=num;
-				//それぞれ何回出たかカウント
-				count[0]++;
-			}else if(num==target[1]){
-				nums[i]=num;
-				count[1]++;
-			}else if(num==target[2]){
-				nums[i]=num;
-				count[2]++;
-			//それ以外は０にする
-			}else{
-				nums[i]=0;
-			}
-		}
-		System.out.println(Arrays.toString(nums));
-		//求める値が出なかった場合
-		if(count[0]==0){
-			System.out.printf("%dは出ませんでした。%n",target[0]);
-		//求める値が出ていたら
-		}else{
-			System.out.printf("%dは%d回出ました。%n",target[0],count[0]);
-		}
-		if(count[1]==0){
-			System.out.printf("%dは出ませんでした。%n",target[1]);
-		}else{
-			System.out.printf("%dは%d回出ました。%n",target[1],count[1]);
-		}
-		if(count[2]==0){
-			System.out.printf("%dは出ませんでした。%n",target[2]);
-		}else{
-			System.out.printf("%dは%d回出ました。%n",target[2],count[2]);
-		}
-	}
+  public static void main(String[] args){
+    //要素数の入力、配列の作成
+    System.out.print("要素数を入力>");
+    int element=new Scanner(System.in).nextInt();
+    int[] nums=new int[element];
+    //カウントする値、カウント数
+    int[] target={1,7,17};
+    int[] count={0,0,0};
+    //乱数作成の繰り返し
+    for(int i=0;i<nums.length;i++){
+      int rand=new Random().nextInt(20)+1;
+      //targetの値以外は０に
+      rand = rand==target[0]||rand==target[1]||rand==target[2]?rand:0;
+      //配列に格納
+      nums[i]=rand;
+      //それぞれ何回出たかカウント
+      for(int j=0;j<target.length;j++){
+        if(nums[i]==target[j]){
+          count[j]++;
+        }
+      }
+    }
+    //numsに格納された値を出力
+    System.out.println(Arrays.toString(nums));
+    for(int i=0;i<count.length;i++){
+      //求める値が出なかった場合
+      if(count[i]==0){
+        System.out.printf("%dは出ませんでした。%n",target[i]);
+      //求める値が出ていたら
+      }else{
+        System.out.printf("%dは%d回出ました。%n",target[i],count[i]);
+      }
+    }
+  }
 }
+
--- java/20201112/Ex2_2.java
+++ java/20201112/Ex2_2.java
@@ -3,33 +3,28 @@ public class Ex2_2{
 	public static void main(String[] args){
 		//要素数の入力、配列の作成
 		System.out.print("要素数を入力>");
-		int youso=new Scanner(System.in).nextInt();
-		int[] nums=new int[youso];
+		int element=new Scanner(System.in).nextInt();
+		int[] nums=new int[element];
 		//カウントする値、カウント数
 		int[] target={1,7,17};
 		int[] count={0,0,0};
 		//乱数作成の繰り返し
 		for(int i=0;i<nums.length;i++){
-			int num=new Random().nextInt(nums.length);
-			//目的の値であるかどうか
-			if(num==target[0]){
-				//配列に格納
-				nums[i]=num;
-				//それぞれ何回出たかカウント
-				count[0]++;
-			}else if(num==target[1]){
-				nums[i]=num;
-				count[1]++;
-			}else if(num==target[2]){
-				nums[i]=num;
-				count[2]++;
-			//それ以外は０にする
-			}else{
-				nums[i]=0;
+			int rand=new Random().nextInt(20)+1;
+			//targetの値以外は０に
+			rand = rand==target[0]||rand==target[1]||rand==target[2]?rand:0;
+			//配列に格納
+			nums[i]=rand;
+			//それぞれ何回出たかカウント
+			for(int j=0;j<target.length;j++){
+				if(nums[i]==target[j]){
+					count[j]++;
+				}
 			}
 		}
+		//numsに格納された値を出力
 		System.out.println(Arrays.toString(nums));
-		for(int i=0;i<nums.length-1;i++){
+		for(int i=0;i<count.length;i++){
 			//求める値が出なかった場合
 			if(count[i]==0){
 				System.out.printf("%dは出ませんでした。%n",target[i]);

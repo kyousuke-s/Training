264bb43b4b9bb9a97710f3085fe0b4004504c646
sato <kyousukesato8@gmail.com>
2020-11-20 15:16:04 +0900

    Q4.java編集

--- java/20201120/Q4.java
+++ java/20201120/Q4.java
@@ -4,11 +4,22 @@ public class Q4{
 		System.out.println("****TriangleCalculator****");
 		while(true){
 			System.out.print("どうする？1…三角形の面積を求める 0…やめる>");
-			System.out.print("底辺を入力してください>");
-			double triBottom=new Scanner(System.in).nextDouble();
-			System.out.print("高さを入力してください>");
-			double triHeight=new Scanner(System.in).nextDouble();
+			int yOrN=new Scanner(System.in).nextInt();
+			if(yOrN==1){
+				System.out.print("底辺を入力してください>");
+				double triBottom=new Scanner(System.in).nextDouble();
+				System.out.print("高さを入力してください>");
+				double triHeight=new Scanner(System.in).nextDouble();
+				double ans=triArea(triBottom,triHeight);
+				System.out.printf("底辺:%.2f,高さ:%.2fの三角形の面積は%.2fです。%n",triBottom,triHeight,ans);
+			}else{
+				System.out.println("アプリケーションを終了します。");
+				return;
+			}
 		}
-
+	
+	}
+	public static double triArea(double a,double b){
+		return (a*b)/2;
 	}
 }
